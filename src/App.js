e1a6df8eac5bacc188a2d4c6e6a9930f9de50094
Aidosdev <vip_aidos_96@mail.ru>
2021-03-12 00:12:08 +0600

    added new components and changed index.css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Layout from './components/Layout/Layout';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! React.js
-        </p>
-      </header>
-    </div>
+    <>
+      <Header title="This is title" descr="This is description!" />
+      <Layout urlBg title="This is title(Image)" descr="This is description!" />
+      <Layout title="This is title(ColorBg)" colorBg="red" descr="This is description!" />
+      <Layout urlBg title="This is title(Image)" descr="This is description!" />
+      <Footer />
+    </>
   );
 }
 
